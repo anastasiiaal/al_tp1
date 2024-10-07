780c12ba45d4996ef4375bc6e5b31ce6882ee3d2
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-10-07 13:54:33 +0200

    add model

---
+++ persistence/models/index.js
@@ -0,0 +1,17 @@
+const sequelize = require('../../infrastructure/config/databaseConfig');
+
+// models import
+const User = require('../../domain/entities/User')
+const Profile = require('../../domain/entities/Profile')
+
+// relations declaration
+// user can have only one role, a role can define many users
+User.belongsTo(Profile, { foreignKey: 'ProfileId' });
+
+// DB sync
+sequelize.sync({alter: true});
+
+module.exports = {
+  User: User,
+  Profile: Profile,
+}

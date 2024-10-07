2a2663c85dc29deed1f0cd2b44f1ff2af6d5d13b
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-10-07 13:58:29 +0200

    add infrastructure/external systems (bdd config)

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // Configuration de express
 const express = require('express');
 const path = require('path');
-const userRouter = require('./routes/user');
 
 // Lecture du fichier .env
 require('dotenv').config()
 
 // Lecture du fichier models/index.js afin de lancer la synchronisation de Sequelize
-require('./models/index.js');
+require('./persistence/models/index.js');
 
 // Importation des routeurs
-const indexRouter = require('./routes/index.js');
+const indexRouter = require('./presentation/routes/index.js');
+const userRoutes = require('./presentation/routes/userRoutes');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-app.use('/user', userRouter);  
+app.use('/user', userRoutes);  
 
 module.exports = app;

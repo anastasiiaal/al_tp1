737a404df844b497b87b9d68b31f449c9b487522
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-10-07 13:59:33 +0200

    delete old files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Exemple de route
-router.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
-
-module.exports = router;

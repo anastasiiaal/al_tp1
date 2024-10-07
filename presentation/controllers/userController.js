5ac8ea9715196f55d4f7a46ede733c9adbd547d4
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-10-07 13:53:21 +0200

    add presentation (controller + routes)

---
+++ presentation/controllers/userController.js
@@ -0,0 +1,49 @@
+const createUser = require('../../application/usecases/createUser');
+const getAllUsers = require('../../application/usecases/getAllUsers');
+const deleteUser = require('../../application/usecases/deleteUser');
+const updateUser = require('../../application/usecases/updateUser');
+
+const userRepository = require('../../persistence/repositories/userRepository');
+const profileRepository = require('../../persistence/repositories/profileRepository');
+
+async function createUserController (req, res) {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+        const user = await createUser({ firstName, lastName, email, password }, userRepository, profileRepository);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la création de l\'utilisateur' });
+    }
+};
+
+async function getAllUsersController (req, res) {
+    try {
+        const users = await getAllUsers(userRepository);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs' });
+    }
+};
+
+async function deleteUserController (req, res) {
+    try {
+        const { id } = req.params;
+        const result = await deleteUser(id, userRepository);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+};
+
+async function updateUserController (req, res) {
+    try {
+        const { id } = req.params;
+        const updateData = req.body;
+        const result = await updateUser(id, updateData, userRepository);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+};
+
+module.exports = { createUserController, getAllUsersController, deleteUserController, updateUserController };

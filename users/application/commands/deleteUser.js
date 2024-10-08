33d79400414d76ec805b77d3174e9964dd5a2b4f
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-10-08 16:10:03 +0200

    delete BankUser (if exists) when deleting User

--- users/application/commands/deleteUser.js
+++ users/application/commands/deleteUser.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 async function deleteUser(id, userRepository) {
     try {
         const user = await userRepository.findByPk(id);
@@ -6,9 +8,20 @@ async function deleteUser(id, userRepository) {
             throw new Error(`Utilisateur avec l'ID ${id} non trouvé.`);
         }
 
-        // Supprimer l'utilisateur via le repository
+        console.log('Tentative de suppression du BankUser avec l\'ID utilisateur :', id);
+        try {
+            const bankUserResponse = await axios.delete(`http://localhost:4000/bankUser/delete/${id}`);
+            if (bankUserResponse.status === 200) {
+                console.log('BankUser supprimé avec succès.');
+            } else {
+                console.log('Aucun BankUser trouvé ou erreur lors de la suppression du BankUser.');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la suppression du BankUser :', error.message);
+        }
+
         await userRepository.delete(id);
-        return { message: 'Utilisateur supprimé avec succès' };
+        return { message: 'Utilisateur et BankUser supprimés avec succès' };
     } catch (error) {
         console.error('Erreur lors de la suppression de l\'utilisateur :', error);
         throw new Error('Erreur lors de la suppression de l\'utilisateur');
